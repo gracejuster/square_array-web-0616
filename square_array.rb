b6b88eae3ae0fce114f08ba54eda33ebf8683751
grahk93 <grace.juster@flatironschool.com>
2016-06-03 10:57:14 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  sqaured_array = []
+  array.each do |item|
+    squared_item = item ** 2
+    sqaured_array.push(squared_item)
+  end
+  return sqaured_array
 end
